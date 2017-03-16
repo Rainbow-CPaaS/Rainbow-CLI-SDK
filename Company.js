1133002759d7d4d435d20c49614620d88a601ec1
Olivier Anguenot <olivier.anguenot@al-enterprise.com>
2017-03-16 21:35:38 +0100

    CLI: List the users of a company

--- Company.js
+++ Company.js
@@ -69,6 +69,33 @@ class Company {
         });
     }
 
+    _getUsersFromCompany(token, id, page) {
+        return new Promise(function(resolve, reject) {
+
+            var offset = "";
+            if(page > -1) {
+                offset = "&offset=";
+                if(page > 1) {
+                    offset += (25 * (page - 1));
+                }
+                else {
+                    offset +=0;
+                }
+            }
+
+            var limit = "";
+            if(page > -1) {
+                limit = "&limit=25";
+            }
+
+            NodeSDK.get('/api/rainbow/admin/v1.0/users?format=full' + offset + limit + '&companyId=' + id, token).then(function(json) {
+                resolve(json);
+            }).catch(function(err) {
+                reject(err);
+            });
+        });
+    }
+
     _getCompany(token, id) {
 
         return new Promise(function(resolve, reject) {
@@ -115,7 +142,11 @@ class Company {
 
         var that = this;
 
-        this._program.command('company')
+        var company = null;
+        var users = null;
+
+        this._program.command('company', '<id>')
+            
             .description("Retrieve information about an existing company")
             .action(function (id) {
                 
@@ -135,8 +166,6 @@ class Company {
                         NodeSDK.start(that._prefs.account.email, that._prefs.account.password).then(function() {
                             return that._getCompany(that._prefs.token, id);
                         }).then(function(json) {
-
-                            var company = json.data;
 
                             status.stop();
                             Screen.print('');
@@ -206,7 +235,98 @@ class Company {
             }
         );
 
-        this._program.command('company delete')
+        this._program.command('users company', '<id>')
+        .description("List the users of a company")
+        .option('-p, --page <n>', 'Display a specific page')
+        .option('-a, --all', 'Display all companies in a single page')
+        .action(function (id) {
+
+            var page = 0;
+            if("page" in that._program) {
+                if(that._program.page > 1) {
+                    page = that._program.page;
+                }
+            }
+        
+            if("all" in that._program && that._program.all) {
+                page = -1
+            }
+
+            Screen.print('Welcome to '.grey + 'Rainbow'.magenta);
+        
+            if(that._prefs.token && that._prefs.user) {
+                Screen.print('You are logged in as'.grey + " " + that._prefs.account.email.magenta);
+                Screen.print('');
+            
+                if ((typeof id !== 'string') || (id.length === 0)) {
+                    Screen.error("An 'id' of a company is required");
+                }
+                else {
+                    Screen.print("Current users from company".white + " '".yellow + id.yellow + "'".yellow);
+                    var status = new Spinner('In progress, please wait...');
+                    status.start();
+                    NodeSDK.start(that._prefs.account.email, that._prefs.account.password).then(function() {
+                        return that._getUsersFromCompany(that._prefs.token, id);
+                    }).then(function(json) {
+
+                        status.stop();
+                        
+                        if(json.total > json.limit) {
+                            var page = Math.floor(json.offset / json.limit) + 1
+                            var totalPage = Math.floor(json.total / json.limit) + 1;
+                            
+                            Screen.print('Displaying Page '.white + page.toString().yellow + " on ".white + totalPage.toString().yellow);
+                        }
+                        Screen.print('');
+
+                        var array = [];
+                        array.push([ "#".gray, "Name".gray, "Account".gray, "Roles".gray, "Active".gray, "ID".gray]);
+                        array.push([ "-".gray, "----".gray, "-------".gray, "-----".gray, "------".gray, "--".gray]);  
+
+                        var users = json.data;
+
+                        for(var i = 0; i < users.length; i++) {
+
+                            var accountType = users[i].accountType;
+                            if(accountType === "free") {
+                                accountType = accountType.white;
+                            }
+                            else {
+                                accountType = accountType.yellow;
+                            }
+
+                            var roles = users[i].roles.join();
+
+                            var active = "true".white;
+                            if(!users[i].isActive) {
+                                active = "false".red;
+                            }
+
+                            array.push([ (i+1).toString().white, users[i].displayName.white, accountType, roles.white, active, users[i].id.white]);  
+                        }
+
+                        var t = table(array);
+                        Screen.table(t);
+                        Screen.print('');
+                        Screen.success(json.total + ' users found.');
+
+                    }).catch(function(err) {
+                        status.stop();
+                        Screen.print('');
+                        Screen.error("Can't delete the company".white + " '" + id.yellow);
+                        if(err.details) {
+                            Screen.print("Error ".red + err.code.toString().gray + " - ".white + err.msg.gray + ": " + err.details.gray);
+                        }
+                        else {
+                            Screen.print("Error ".red + err.code.toString().gray + " - ".white + err.msg.gray);
+                        }
+                    });
+                }
+            }
+
+        });
+
+        this._program.command('company delete', '<id>')
             .description("Delete an existing company")
             .action(function (id) {
 
@@ -245,7 +365,7 @@ class Company {
             }
         );
 
-        this._program.command('company create')
+        this._program.command('company create', '<name>')
             .description("Create a new company")
             .action(function (name) {
 
