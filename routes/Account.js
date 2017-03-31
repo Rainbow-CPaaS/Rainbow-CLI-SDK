80916290c8c3014ea0ad1ac1ec6e3b620946057d
Olivier Anguenot <olivier.anguenot@al-enterprise.com>
2017-03-31 21:35:41 +0200

    CLI: Add example in help

--- routes/Account.js
+++ routes/Account.js
@@ -25,6 +25,14 @@ class Account {
         this._program.command('login', '<email> <password>')
             .description("Log-in to Rainbow")
             .option('-h, --host <hostname>', "Log-in to a specific host. 'hostname' can be 'official' or any hostname. If no --host, 'sandbox' is used")
+            .on('--help', function(){
+                console.log('  Examples:');
+                console.log('');
+                console.log('    $ rbw login johndoe@mycompany.com Password_12345');
+                console.log('    $ rbw login johndoe@mycompany.com Password_12345 --host official');
+                console.log('    $ rbw login johndoe@mycompany.com Password_12345 --host openrainbow.com');
+                console.log('');
+            })
             .action(function (email, password, commands) {
 
             var platform = commands.host ? commands.host : "sandbox";
@@ -34,6 +42,12 @@ class Account {
 
         this._program.command('logout')
             .description("Log-out to Rainbow")
+            .on('--help', function(){
+                console.log('  Examples:');
+                console.log('');
+                console.log('    $ rbw logout');
+                console.log('');
+            })
             .action(function () {
 
             that._account.logout();
@@ -41,6 +55,12 @@ class Account {
         
         this._program.command('whoami')
             .description("Display information about the connected user")
+            .on('--help', function(){
+                console.log('  Examples:');
+                console.log('');
+                console.log('    $ rbw whoami');
+                console.log('');
+            })
             .action(function (command) {
 
             that._account.getConnectedUserInformation();
