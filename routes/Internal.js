eba812c28d4d21c02bc5b3f204075073df9b174a
Olivier Anguenot <oanguenot@gmail.com>
2018-09-05 09:59:03 +0200

    Get metrics for current month if not specified

--- routes/Internal.js
+++ routes/Internal.js
@@ -23,8 +23,8 @@ class Internal {
     listOfCommands() {
         var that = this;
 
-        this._program.command('dashboard metrics')
-        .description("Display applications metrics for application with subscription")
+        this._program.command('dashboard applications')
+        .description("Display pay-as-you-go applications metrics")
         .option('-m, --month <month>', 'Get metrics for a specific month. Format is YYYYMM')
         .option('-a, --all', 'Get metrics for all applications including business applications')
         .option('-f, --file <filename>', 'Print result to a file in CSV')
@@ -42,11 +42,68 @@ class Internal {
                     owner: commands.owner,
                 };
 
-                that._internal.dashboardMetrics(options);
+                that._internal.dashboardApplications(options);
             }).catch( (err) => {
                 Message.error(err, {});
             });
         });
+
+        this._program.command('dashboard developers')
+        .description("Display developers metrics ")
+        .option('-p, --page <number>', 'Display a specific page')
+        .option('-l, --limit <number>', 'Limit to a number of instances per page (max=1000)')
+        .option('-m, --max', 'Same as --limit 1000')
+        //.option('-m, --month <month>', 'Get metrics for a specific month. Format is YYYYMM')
+        .option('-f, --file <filename>', 'Print result to a file in CSV')
+        //.option('-o, --owner <ownerid>', 'Filter by owner of applications')
+        .option('-v, --verbose', 'Use verbose console mode')
+        .action(function (commands) {
+            Middleware.parseCommand(commands).then( () => {
+                Logger.isActive = commands.verbose || false;
+
+                var options = {
+                    csv: "",
+                    format: "full",
+                    page: "1",
+                    limit: "25"
+                };
+
+                var page = "1";
+                var limit = "25";
+                var format = "full";
+
+                if("page" in commands) {
+                    if(commands.page > 1) {
+                        page = commands.page;
+                    }
+                }
+
+                if("limit" in commands && commands.limit) {
+                    if(commands.limit > 0) {
+                        limit = commands.limit;
+                    }
+                }
+
+                options = {
+                    noOutput: commands.json || false,
+                    month: commands.month,
+                    csv: commands.file || "",
+                    format: format,
+                    page: page,
+                    limit: limit
+                };
+
+                if(commands.max) {
+                    options.limit = 1000;
+                }
+
+                that._internal.dashboardDevelopers(options);
+            }).catch( (err) => {
+                Message.error(err, {});
+            });
+        });
+
+
     }
 }
 
