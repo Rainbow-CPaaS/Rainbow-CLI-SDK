a77d54d2905e4b860865870fc4d7f92b3681ac8d
Olivier Anguenot <olivier.anguenot@al-enterprise.com>
2019-02-20 10:09:59 +0100

    Prepare tests for being able to tests on an alternative platform than sandbox

---
+++ afterbuild_sandbox.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#Sandbox user account
+# login: org_admin@rainbowcli-tests.com"
+# password: Password_123
+# company: RainbowCLI-tests
+# rights: org_admin + app_superadmin
+
+#Variables for testing on sandbox
+export AFTERBUILD_LOGIN="org_admin@rainbowcli-tests.com"
+export AFTERBUILD_PASSWORD="Password_123"
+export AFTERBUILD_HOST="sandbox.openrainbow.com"
+
+#Launch tests on Sandbox
+zunit
+
