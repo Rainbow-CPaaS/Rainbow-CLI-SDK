bf8c8cfa41b9014b64aaed50a6b02acd8049ea1b
Olivier Anguenot <olivier.anguenot@al-enterprise.com>
2019-03-07 14:47:45 +0100

    Allow to run tests on cpaas-preprod

---
+++ afterbuild_cpaaspreprod.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#.Net user account
+# login: org_admin@rainbowcli-tests.com"
+# password: Password_123
+# company: RainbowCLI-tests
+# rights: org_admin + app_admin
+
+#Variables for testing on sandbox
+export AFTERBUILD_LOGIN="org_admin@rainbowcli-tests.com"
+export AFTERBUILD_PASSWORD="Password_123"
+export AFTERBUILD_HOST="cpaaspreprod.openrainbow.net"
+
+#Launch tests on .net
+zunit
+
